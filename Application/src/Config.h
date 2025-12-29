9300f7ed4ba6fdaba374660a68116339217a9f32
TerraCrafterE3 <terracraftere3@gmail.com>
2025-12-29 07:22:22 +0200

    Added argument parsing

---
+++ Application/src/Config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+struct Config{
+    static constexpr const char* APP_NAME = "CLI-Template";
+    static constexpr const char* APP_VERSION = "1.0.0";
+    bool verbose = false;
+};
--- Application/src/main.cpp
+++ Application/src/main.cpp
@@ -1,6 +1,23 @@
-#include <stdio.h>
+#include <iostream>
+#include <argparse/argparse.hpp>
 
-int main () {
-    printf("Hello, World!\n");
+#include "Config.h"
+
+int main (int argc, char** argv) {
+    Config conf;
+
+    argparse::ArgumentParser program("CLI-App");
+    program.add_argument("-v", "--verbose")
+        .help("increase output verbosity")
+        .default_value(false)
+        .implicit_value(true);
+    try {
+        program.parse_args(argc, argv);
+    } catch (const std::runtime_error& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << program.help().str() << std::endl;
+        return 1;
+    }
+    conf.verbose = program.get<bool>("--verbose");
     return 0;
 }
